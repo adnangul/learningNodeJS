5035bbafb9c83d7ee849d7db08440c0b4890bdcd
Adnan Yaqoob <itsadnan@gmail.com>
2023-01-05 14:43:58 -0600

    AuthGuard + unit tests

--- NestJS/mycar/src/users/users.controller.ts
+++ NestJS/mycar/src/users/users.controller.ts
@@ -9,6 +9,7 @@ import {
   Query,
   NotFoundException,
   Session,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -17,6 +18,8 @@ import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
+import { User } from './user.entity';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -26,13 +29,9 @@ export class UsersController {
     private authService: AuthService,
   ) {}
 
-  // @Get('/whoami')
-  // whoAmI(@Session() session: any) {
-  //   return this.usersService.findOne(session.userId);
-  // }
-
   @Get('/whoami')
-  whoAmI(@CurrentUser() user: string) {
+  @UseGuards(AuthGuard)
+  whoAmI(@CurrentUser() user: User) {
     return user;
   }
 
--- NestJS/mycar/src/users/users.service.spec.ts
+++ NestJS/mycar/src/users/users.service.spec.ts
@@ -1,12 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { plainToClass } from 'class-transformer';
+import { Repository } from 'typeorm';
+
 import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 describe('UsersService', () => {
   let service: UsersService;
+  // let userRepository: Partial<Repository<User>> ;
 
   beforeEach(async () => {
+    const user: User = { id: 1, email:'ab@ac.com', password: 'asdf' } as User;
+    // const user: User = plainToClass(User, { id: 1, email:'ab@ac.com', password: 'asdf' });
+
+    // mo
+    const mockedRepo = {
+      // mock the repo `findOneOrFail`
+      findOneOrFail: jest.fn((id) => Promise.resolve(user)),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [UsersService,
+        // Mock the repository using the `getRepositoryToken` from @nestjs/typeorm
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockedRepo,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
